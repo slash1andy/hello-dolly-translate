0ae22736ca5ec6e772c2db0a4d1c1ffa66887f31
RiaanKnoetze <riaan@woothemes.com>
2016-09-19 10:37:10 -0700

    Convert variable to array.

--- hello.php
+++ hello.php
@@ -8,52 +8,54 @@ Plugin Name: Hello Dolly
 Plugin URI: https://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Matt Mullenweg
-Version: 1.6
+Version: 2.0
 Author URI: http://ma.tt/
 */
 
 function hello_dolly_get_lyric() {
 	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
+$lyrics = array(
+	"Hello, Dolly",
+	"Well, hello, Dolly",
+	"It's so nice to have you back where you belong",
+	"You're lookin' swell, Dolly",
+	"I can tell, Dolly",
+	"You're still glowin', you're still crowin",
+	"You're still goin' strong",
+	"We feel the room swayin",
+	"While the band's playin",
+	"One of your old favourite songs from way back when",
+	"So, take her wrap, fellas",
+	"Find her an empty lap, fellas",
+	"Dolly'll never go away again",
+	"Hello, Dolly",
+	"Well, hello, Dolly",
+	"It's so nice to have you back where you belong",
+	"You're lookin' swell, Dolly",
+	"I can tell, Dolly",
+	"You're still glowin', you're still crowin'",
+	"You're still goin' strong",
+	"We feel the room swayin'",
+	"While the band's playin'",
+	"One of your old favourite songs from way back when",
+	"Golly, gee, fellas",
+	"Find her a vacant knee, fellas",
+	"Dolly'll never go away",
+	"Dolly'll never go away",
+	"Dolly'll never go away again",
+);
 
 	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
+	shuffle($lyrics);
 
-	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
+	return $lyrics[0];
+
 }
 
 // This just echoes the chosen line, we'll position it later
 function hello_dolly() {
 	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>$chosen</p>";
+	echo "<p id='dolly'>" . $chosen . "</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
