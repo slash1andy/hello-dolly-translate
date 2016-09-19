ea6ad869fb8abf7865fef0eaad0121e88689fa7f
RiaanKnoetze <riaan@woothemes.com>
2016-09-19 10:45:22 -0700

    Add wptexturize

--- hello.php
+++ hello.php
@@ -55,7 +55,7 @@ $lyrics = array(
 // This just echoes the chosen line, we'll position it later
 function hello_dolly() {
 	$chosen = hello_dolly_get_lyric();
-	echo "<p id='dolly'>" . $chosen . "</p>";
+	echo "<p id='dolly'>" . wptexturize ( $chosen ) . "</p>";
 }
 
 // Now we set that function up to execute when the admin_notices action is called
